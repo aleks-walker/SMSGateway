0d0a5d26ad2a1d2da36d44e4b0c1051fa6721183
Aleksei Guliaev <guliaevaleksei@gmail.com>
2020-09-21 13:59:31 +0600

    Initial commit

---
+++ app/src/main/java/kg/kloop/android/smsgateway/MainActivity.kt
@@ -0,0 +1,45 @@
+package kg.kloop.android.smsgateway
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private val RC_SIGN_IN: Int = 200
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build())
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, "User is ${user?.displayName}", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
