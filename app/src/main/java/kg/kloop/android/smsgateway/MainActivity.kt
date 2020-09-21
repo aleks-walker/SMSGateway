18afc7baa9a95fd5e03f6f4f971d8b25059c3627
Aleksei Guliaev <guliaevaleksei@gmail.com>
2020-09-21 14:11:42 +0600

    Add databinding to main activity

--- app/src/main/java/kg/kloop/android/smsgateway/MainActivity.kt
+++ app/src/main/java/kg/kloop/android/smsgateway/MainActivity.kt
@@ -5,22 +5,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import kg.kloop.android.smsgateway.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val RC_SIGN_IN: Int = 200
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build())
 
-        // Create and launch sign-in intent
         startActivityForResult(
             AuthUI.getInstance()
                 .createSignInIntentBuilder()
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity() {
 
             if (resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                Toast.makeText(this, "User is ${user?.displayName}", Toast.LENGTH_SHORT).show()
+                binding.textView.text = user?.displayName
             } else {
                 Toast.makeText(this, "Fail", Toast.LENGTH_SHORT).show()
             }
